08b7443d096d120dda9cc75d54af6a61b0f65362
alex.hill@gmail.com <alex.hill@gmail.com>
2024-08-21 16:58:08 +0100

    condition message

--- R/utils.R
+++ R/utils.R
@@ -1,10 +1,13 @@
 with_warnings <- function(expr) {
   my_warnings <- NULL
+
   w_handler <- function(w) {
     my_warnings <<- c(my_warnings, list(w))
     invokeRestart("muffleWarning")
   }
+
   val <- withCallingHandlers(expr, warning = w_handler)
-  list(output = val, warnings = lapply(my_warnings,
-                                       function(w) jsonlite::unbox(w$message)))
+  list(output = val,
+       warnings = lapply(my_warnings,
+                         function(w) jsonlite::unbox(conditionMessage(w))))
 }
